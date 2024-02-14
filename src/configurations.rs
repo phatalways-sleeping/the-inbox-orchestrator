71b3b908754833001b16f1041388547286af19b5
phatalways-sleeping <trankim147@gmail.com>
2024-02-14 15:05:41 +0700

    CD setup

--- src/configurations.rs
+++ src/configurations.rs
@@ -4,10 +4,10 @@ use secrecy::ExposeSecret;
 use secrecy::Secret;
 use serde::Deserialize;
 use std::convert::TryFrom;
-
+use std::env;
 #[derive(Deserialize)]
 pub struct Settings {
-    pub application_port: u16,
+    pub application: ApplicationSettings,
     pub database: DatabaseSettings,
 }
 
@@ -18,6 +18,12 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+#[derive(Deserialize)]
+pub struct ApplicationSettings {
+    pub host: String,
+    pub port: u16,
 }
 
 impl DatabaseSettings {
@@ -44,12 +50,27 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, ConfigError> {
-    let settings_builder = Config::builder();
+    let mut settings_builder = Config::builder();
 
-    match settings_builder
-        .add_source(config::File::with_name("configuration"))
-        .build()
-    {
+    let base_path = std::env::current_dir().expect("Fail to read the base directory");
+
+    let configuration_path = base_path.join("configuration");
+
+    // Read the base configuration
+    settings_builder =
+        settings_builder.add_source(config::File::from(configuration_path.join("base")));
+
+    // Check if we are in production environment
+    let environment: Environment = env::var("ENVIRONMENT")
+        .unwrap_or("local".into())
+        .try_into()
+        .expect("Fail to parse environment");
+
+    settings_builder = settings_builder.add_source(config::File::from(
+        configuration_path.join(environment.as_str()),
+    ));
+
+    match settings_builder.build() {
         Ok(config) => {
             let settings: Settings = config.try_into()?;
             Ok(settings)
@@ -58,17 +79,46 @@ pub fn get_configuration() -> Result<Settings, ConfigError> {
     }
 }
 
+enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Self::Local => "local",
+            Self::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            e => Err(format!(
+                "{} is not supported. Use either local or production.",
+                e
+            )),
+        }
+    }
+}
+
 impl TryFrom<Config> for Settings {
     type Error = ConfigError;
 
     fn try_from(config: Config) -> Result<Self, Self::Error> {
-        if config.get::<u16>("application_port").is_err()
+        if config.get::<ApplicationSettings>("application").is_err()
             || config.get::<DatabaseSettings>("database").is_err()
         {
             return Err(ConfigError::Message(String::from("Not enough field")));
         }
         Ok(Self {
-            application_port: config.get("application_port").unwrap(),
+            application: config.get("application").unwrap(),
             database: config.get("database").unwrap(),
         })
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
-use std::net::TcpListener;
+use sqlx::postgres::PgPoolOptions;
+use std::{net::TcpListener, time::Duration};
 use zero2prod::{
     configurations::get_configuration,
     run,
@@ -12,9 +12,13 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Cannot read configuration");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let db_pool = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let db_pool: sqlx::Pool<sqlx::Postgres> = PgPoolOptions::new()
+        .acquire_timeout(Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
         .expect("Fail to connect to database");
     let listener = TcpListener::bind(address).expect("Fail to bind to a port");
     run(listener, db_pool)?.await
